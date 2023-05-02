9dcb85bbbbfbab40b2e16bae5b238b7761947d61
Ruan Pablo Fernandes Silva <128431007+RuanPSilva@users.noreply.github.com>
2023-05-02 11:52:15 -0300

    código do canvas
    código básico para chamar o canvas

---
+++ script.js
@@ -0,0 +1,5 @@
+const canva = document.getElementById('canvas'); // chamando o elemento canvas
+const ctx = canvas.getContext('2d'); // definindo se é 2d ou 3d o que irá aparecer na tela
+ctx.FillStyle = 'purple'; // definir cor
+ctx.FillRect (10,10,150,100); // medidas
+
