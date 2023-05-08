7f8eb16386781b10ffc78c15f04283e9f3f5be67
RuanPSilva <ruan.silva125@etec.sp.gov.br>
2023-05-08 12:12:27 -0300

    codigos de formas do canvas

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const canva = document.getElementById('canvas'); // chamando o elemento canvas
-const ctx = canvas.getContext('2d'); // definindo se é 2d ou 3d o que irá aparecer na tela
-ctx.FillStyle = 'purple'; // definir cor
-ctx.FillRect (10,10,150,100); // medidas
+const canva = document.getElementById('canvas');
+const ctx = canvas.getContext('2d'); 
+ctx.fillStyle = 'purple'; 
+ctx.fillRect (10,10,150,100); 
+ctx.strokeRect (10,10,150,100);
 
+
+const canva2 = document.getElementById('canvas2');
+const ctx2 = canvas2.getContext('2d');
+ctx2.fillStyle = "red";
+ctx2.beginPath();
+ctx2.moveTo(75,50);
+ctx2.lineTo(100,75);
+ctx2.lineTo(100,25);
+ctx2.fillStyle();
