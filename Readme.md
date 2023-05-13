a184f7417e6d353578b2eaccd57fe9f96819151a
MariSantiago0 <127640439+MariSantiago0@users.noreply.github.com>
2023-05-13 12:11:45 -0300

    arrumando os codigos

--- Readme.md
+++ Readme.md
@@ -1,10 +1,36 @@
-EXPLICANDO O CÓDIGO 
+<h1> Projeto de Desenho de Formas no Canvas </h1>
 
-# índice
+<h2> # índice </h2>
+<ul>
+  <li>Descrição do Projeto</li>
+  <li>Estrutura do Projeto</li>
+  <li>Formas do Canvas utilizadas</li>
+</ul>
 
-*[descrição do Projeto](#descrição-do-projeto)
-*[Estrutura HTML](#estrutura-html)
-*[Formas no Canvas usando javascript](#formas-no-canvas-usando-javascript)
-*[Enfeitando usando CSS](#enfeitando-usando-css)
-*[Pessoas ]
+<br>
+
+<h3> Descrição do projeto </h3>
+ <p> Este é um projeto de exemplo que demonstra como criar e desenhar formas no elemento <em><strong><canvas></strong></em>  utilizando JavaScript. O objetivo é fornecer uma compreensão básica de como utilizar o Canvas para criar formas gráficas e imagens. </p>
+  
+  <br>
+  
+<h3> Estrutura do Projeto </h3>
+  <p> 
+   - index.html -> Contém o elemento <canvas> e carrega o arquivo JavaScript. <br>
+   - script.js -> Contém o código para criar e desenhar as formas no canvas. <br>
+   - img/punk.png -> Pasta que contém a imagem. <br>
+   - css.style -> Contém o código que arruma a imagem
+  </p>
+  
+  <br>
+
+<h3> Formas do Canvas utilizadas </h3>
+<p> Retângulo: Demonstração de como criar um retângulo preenchido e contornado no canvas. <br>
+  > <em>Círculo</em>: Demonstração de como criar um círculo preenchido no canvas. <br> 
+  > <em>Linhas</em>: Demonstração de como desenhar linhas retas e curvas usando os métodos <em><strong>moveTo()t</strong></em> e <em><strong>lineTo()</strong></em>. <br> 
+  > <em>Triângulo</em>: Demonstração de como desenhar um triângulo preenchido no canvas. <br> 
+  > <em>Curva de Bezier</em>: Demonstração de como desenhar uma curva de Bezier usando o método  <em><strong>bezierCurveTo()</strong></em>. <br>
+  > <em>Imagem</em>: Demonstração de como exibir uma imagem no canvas utilizando o método  <em><strong>drawImage()</strong></em>. </p>
+  
+
 
--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
 const canva = document.getElementById('canvas'); //buscando o elemento do documento HTML com o id "canvas"
 const ctx = canvas.getContext('2d'); // obtendo o contexto 2d para desenhar na tela em 2d 
 ctx.strokeStyle = "aquamarine"; // definindo a cor do contorno do desenho que é aquamarine
 ctx.fillStyle = "white"; // preenchendo a cor branca dentro no desenho
 ctx.lineWidth = 2; // tamanho da linha do contorno do desenho
-ctx.fillRect (50,50,150,50); //  estamos usando o contexto 2D para desenhar um retângulo preenchido. Os quatro parâmetros representam a posição e as dimensões do retângulo. No caso, estamos desenhando um retângulo preenchido nas coordenadas (50, 50) -> (x,y) com largura 150 e altura 50.
-ctx.strokeRect (50,50,150,50); // para desenhar o contorno do desenho usando as mesmas coordenadas do preenchimento
-=======
-const canva = document.getElementById('canvas');
-const ctx = canvas.getContext('2d'); 
-ctx.strokeStyle = "aquamarine";
-ctx.fillStyle = "white";
-ctx.lineWidth = 2;
-ctx.fillRect (50,50,150,50); 
-ctx.strokeRect (50,50,150,50);
->>>>>>> 516374dba719595ce9ae373f8eabc327b0034409
+ctx.fillRect (65,55,95,65); //  estamos usando o contexto 2D para desenhar um retângulo preenchido. Os quatro parâmetros representam a posição e as dimensões do retângulo. No caso, estamos desenhando um retângulo preenchido nas coordenadas (50, 50) -> (x,y) com largura 150 e altura 50.
+ctx.strokeRect (65,55,95,65); // para desenhar o contorno do desenho usando as mesmas coordenadas do preenchimento
 
 
 const canva2 = document.getElementById('canvas2');
@@ -28,7 +18,7 @@ ctx2.fill(); // vai preencher com cor esse contorno, para que fique visivel
 
 const canva3 = document.getElementById('canvas3');
 const ctx3 = canva3.getContext('2d');
-ctx3.strokeStyle = "plum";
+ctx3.strokeStyle = "royalblue";
 ctx3.lineWidth = 4;
 ctx3.beginPath(); // marcando o caminho do desenho. O caminho é uma sequencia de comandos especificando como o desenho será desenhado conforme as coordenadas
 ctx3.arc(75, 75, 65, 0, Math.PI*2, false); // desenhar um arco em um elemento canvas. 
@@ -45,21 +35,18 @@ ctx4.stroke();
 
 const canva5 = document.getElementById('canvas5');
 const ctx5 = canva5.getContext('2d');
-ctx5.strokeStyle = "gold";
+ctx5.strokeStyle = "steelblue";
 ctx5.lineWidth = 2;
 ctx5.moveTo(50, 50);
 ctx5.bezierCurveTo(100, 0, 200, 200, 250, 50); //  desenha uma curva de Bezier cúbica em um elemento canvas. 
 ctx5.stroke();
 
 const canva6img = document.getElementById('canvas6img');
-const ctx6 = canva6img.getContext("2d");
+const ctx6 = canva6img.getContext('2d');
 const img = new Image(); //  criando um novo objeto de imagem usando o construtor Image(). Esse objeto de imagem será usado para carregar e exibir a imagem no canvas.
 img.src = "img/punk.png"; // chamando a imagem na pasta
 img.onload = function () { // função criada para desenhar a imagem na tela
   ctx6.drawImage(img, 0, 0); // desenhando a imagem conforme as coordenadas
 };
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 516374dba719595ce9ae373f8eabc327b0034409
